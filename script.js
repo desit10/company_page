ac8ffa8b1afb75fb4db5bd261f69dbf8995ab4dd
Sergey Ponomarev <ser_0.9.0@mail.ru>
2023-07-01 23:54:13 +0300

    company page

---
+++ script.js
@@ -0,0 +1,8 @@
+// 'use strict';
+// let two = document.querySelector('.box__services').style.top;
+// let two1 = document.querySelector('.box__services').style.top;
+// let a = 100;
+// window.addEventListener('scroll', function () {
+//   console.log(this.scrollY + 'px');
+//   document.querySelector('.box__services').style.top = '0';
+// });
